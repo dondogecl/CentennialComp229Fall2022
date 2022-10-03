680f133fd701c06684813609283fd1b6704a5f6e
Alejandro A <ale@doges.dev>
2022-10-02 22:53:16 -0400

    created Dev and Prod environment declarations in config/express.js

--- Assignment01_Portfolio/config/express.js
+++ Assignment01_Portfolio/config/express.js
@@ -1,13 +1,29 @@
 // express application object
 // imports of modules
-const express = require('express');
-const morgan = require('morgan');
-const compress = require('compression');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
+const express = require("express");
+const morgan = require("morgan");
+const compress = require("compression");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
-module.exports = function() {
-    let app = express();
-    require('../app/routes/index.server.routes.js')(app);
-    return app;
-}
+module.exports = function () {
+  let app = express();
+  // identify development environment
+  if (process.env.NODE_ENV === "development") {
+    // enables logs
+    app.use(morgan("dev"));
+  } else if (process.env.NODE_ENV === "production") {
+    app.use(compress());
+  }
+  // parser
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+
+  require("../app/routes/index.server.routes.js")(app);
+  return app;
+};
